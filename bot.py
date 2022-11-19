e77bd43991875ab5482cdc84a4ea152b0566a82f
Lx <116142186+0AIB@users.noreply.github.com>
2022-11-19 12:38:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+mport asyncio
 import logging
 logging.basicConfig(
     level=logging.DEBUG,
@@ -6,11 +7,12 @@ logging.basicConfig(
 logging.getLogger().setLevel(logging.INFO)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
+logger = logging.getLogger(__name__)
+
 import uvloop
 uvloop.install()
 from config import Config
-from pyrogram import Client 
-
+from pyrogram import Client, filters
 
 class channelforward(Client, Config):
     def __init__(self):
@@ -31,6 +33,22 @@ class channelforward(Client, Config):
     async def stop(self):
         await super().stop()
         print("Session stopped. Bye!!")
+
+
+@channelforward.on_message(filters.channel)
+async def forward(client, message):
+    # Forwarding the messages to the channel
+   try:
+      for id in Config.CHANNEL:
+         from_channel, to_channel = id.split(":")
+         if message.chat.id == int(from_channel):
+#           func = message.copy if False else message.forward
+            await message.copy(int(to_channel))
+            print("Forwarded a message from", from_channel, "to", to_channel)
+            await asyncio.sleep(1)
+   except Exception as e:
+      logger.exception(e)
+
 
 
 if __name__ == "__main__" :
